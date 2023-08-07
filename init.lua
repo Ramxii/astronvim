67f19cafe597affdd9a82948a64590f201a70422
ramxie <ramziemontoute@gmail.com>
2023-08-07 19:36:46 -0400

    update

--- init.lua
+++ init.lua
@@ -1,81 +1,47 @@
 -- Go to for config help
 -- https://github.com/AstroNvim/user_example/tree/main
-
 return {
-  -- Theme
-  colorscheme = "monokai-pro",
+   -- Theme
+   colorscheme = "monokai-pro",
 
-  -- Plugins
-  plugins = {
-    {
-      "loctvl842/monokai-pro.nvim",
-      config = function()
-        require("monokai-pro").setup({
-          transparent_background = false,
-          terminal_colors = true,
-          devicons = true, -- highlight the icons of `nvim-web-devicons`
-          styles = {
-            comment = { italic = true },
-            keyword = { italic = true }, -- any other keyword
-            type = { italic = true }, -- (preferred) int, long, char, etc
-            storageclass = { italic = true }, -- static, register, volatile, etc
-            structure = { italic = true }, -- struct, union, enum, etc
-            parameter = { italic = true }, -- parameter pass in function
-            annotation = { italic = true },
-            tag_attribute = { italic = true }, -- attribute of tag in reactjs
-          },
-          filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
-          -- Enable this will disable filter option
-          day_night = {
-            enable = false, -- turn off by default
-            day_filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
-            night_filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
-          },
-          inc_search = "background", -- underline | background
-          background_clear = {
-            -- "float_win",
-            "toggleterm",
-            "telescope",
-            -- "which-key",
-            "renamer",
-            "notify",
-            -- "nvim-tree",
-            -- "neo-tree",
-            -- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
-          },-- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
-          plugins = {
-            bufferline = {
-              underline_selected = false,
-              underline_visible = false,
-            },
-            indent_blankline = {
-              context_highlight = "default", -- default | pro
-              context_start_underline = false,
-            },
-          },
-          ---@param c Colorscheme
-          override = function(c) end,
-        })
-      end,
+   lsp = {
+      servers = {
+         "clangd",
+      },
+      config = {
+         clangd = {
+            capabilities = {
+               offsetEncoding = "utf-16",
+        },
+      },
     },
-    {
-      "iamcco/markdown-preview.nvim",
-      event = "BufRead",
-      config = function()
-        vim.fn["mkdp#util#install"]()
-      end,
-    },
-    {
-      "arnamak/stay-centered.nvim",
-      event = "BufRead",
-      config = function()
-        require("stay-centered").setup()
-      end,
-    }
   },
 
-  -- Icons
-  icons = {
-    GitUntracked = " ",
-  }
+   -- Icons
+   icons = {
+     GitUntracked = " ",
+   },
+
+   -- Plugins
+   plugins = {
+
+      {
+         "loctvl842/monokai-pro.nvim",
+         config = function()
+         require("monokai-pro").setup({
+            filter = "classic", -- classic | octagon | pro | machine | ristretto | spectrum
+            background_clear = {"toggleterm", "float_win"}
+         })
+         end
+      },
+    {
+      "rebelot/heirline.nvim",
+      opts = function(_, opts)
+        opts.winbar = nil
+        return opts
+      end
+    },
+    
+   },
+
 }
